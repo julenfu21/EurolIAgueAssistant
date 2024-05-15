809cae479b4a9970a4ae55f5b2c049c7d7dca8b0
Joseba Bertolini <jbertolini001@ikasle.ehu.eus>
2024-05-15 21:39:29 +0200

    ERROR Player_Stats

--- actions/actions.py
+++ actions/actions.py
@@ -2,6 +2,7 @@ from typing import Any, Text, Dict, List
 
 from euroleague_api.standings import Standings
 from euroleague_api.team_stats import TeamStats
+from euroleague_api.player_stats import PlayerStats
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet, AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
@@ -97,7 +98,7 @@ class ActionReturnRequestedTeamStatsAll(Action):
             competition = 'U'
 
         team_stats = TeamStats(competition=competition)
-        df = team_stats.get_team_stats(endpoint='traditional')
+        df = team_stats.get_team_stats_all_seasons(endpoint='traditional')
 
         # Poner más stats? Pedir cuales quiere el usuario?
         desired_row = df[df['team.name'] == team_name]
@@ -117,4 +118,58 @@ class ActionReturnRequestedTeamStatsAll(Action):
         )
 
         # return [SlotSet("team", None)]
+        return [AllSlotsReset()]
+    
+
+class ActionReturnRequestedPlayerStatsAll(Action):
+
+    def name(self) -> Text:
+        return "action_return_requested_player_stats_all"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        player_name = tracker.get_slot(key="player")
+        # Split the player name into first name and last name
+        name_parts = player_name.split()
+        first_name = name_parts[0]
+        last_name = name_parts[1]
+
+        # Format the player name as SURNAME, NAME
+        formatted_name = f"{last_name.upper()}, {first_name.upper()}"
+
+        competition = tracker.get_slot(key="competition")
+        if competition == 'Euroleague':
+            competition = 'E'
+        else:
+            competition = 'U'
+
+        player_stats = PlayerStats(competition=competition)
+        df = player_stats.get_player_stats_all_seasons(endpoint='traditional')
+
+        # Poner más stats? Pedir cuales quiere el usuario?
+        desired_row = df[df['player.name'] == formatted_name]
+        games_played = desired_row['gamesPlayed'].iloc[0]
+        points = desired_row['pointsScored'].iloc[0]
+        assists = desired_row['assists'].iloc[0]
+        rebounds = desired_row['totalRebounds'].iloc[0]
+        steals = desired_row['steals'].iloc[0]
+        blocks = desired_row['blocks'].iloc[0]
+        turnovers = desired_row['turnovers'].iloc[0]
+        pir = desired_row['pir'].iloc[0]
+        teams_played_for = desired_row['player.team.name'].to_string(index=False).split(';')
+        teams_played_for_string = ', '.join(teams_played_for)
+
+
+        dispatcher.utter_message(
+            text=f"Player stats per game for {formatted_name} in teams {teams_played_for_string}: \n Games played: {games_played} \n Points: {points} \n "
+                 f"Assists: {assists} \n Rebounds: {rebounds} \n Steals: {steals} \n Blocks: {blocks} \n "
+                 f"Turnovers: {turnovers} \n Pir: {pir}"
+        )
+
+        # return [SlotSet("player", None)]
         return [AllSlotsReset()]
