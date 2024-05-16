1b5b4afa916cafdb315b7fb4e75c45a70ea924f6
Julen <jfuentes019@ikasle.ehu.eus>
2024-05-16 02:59:42 +0200

    Merge player stats of all seasons and leaders

--- actions/actions.py
+++ actions/actions.py
@@ -229,6 +229,65 @@ class ActionReturnRequestedPlayerStatsAll(Action):
 
         # return [SlotSet("player", None)]
         return [AllSlotsReset()]
+
+
+class ActionReturnRequestedPlayerStatsSingleSeason(Action):
+
+    def name(self) -> Text:
+        return "action_return_requested_player_stats_single_season"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        player_name = tracker.get_slot(key="player")
+        season = tracker.get_slot(key="season")
+        # Split the player name into first name and last name
+        name_parts = player_name.split()
+        first_name = name_parts[0]
+        last_name = name_parts[1]
+
+        # Format the player name as SURNAME, NAME
+        formatted_name = f"{last_name.upper()}, {first_name.upper()}"
+
+        competition = tracker.get_slot(key="competition")
+        if competition == 'Euroleague':
+            competition = 'E'
+        else:
+            competition = 'U'
+
+        player_stats = PlayerStats(competition=competition)
+        df = player_stats.get_player_stats_single_season(
+            endpoint='traditional',
+            season=season,
+            phase_type_code=None,
+            statistic_mode="PerGame"
+        )
+
+        # Poner más stats? Pedir cuales quiere el usuario?
+        desired_row = df[df['player.name'] == formatted_name]
+        games_played = desired_row['gamesPlayed'].iloc[0]
+        points = desired_row['pointsScored'].iloc[0]
+        assists = desired_row['assists'].iloc[0]
+        rebounds = desired_row['totalRebounds'].iloc[0]
+        steals = desired_row['steals'].iloc[0]
+        blocks = desired_row['blocks'].iloc[0]
+        turnovers = desired_row['turnovers'].iloc[0]
+        pir = desired_row['pir'].iloc[0]
+        teams_played_for = desired_row['player.team.name'].to_string(index=False).split(';')
+        teams_played_for_string = ', '.join(teams_played_for)
+
+        dispatcher.utter_message(
+            text=f"Player stats per game for {formatted_name} in teams {teams_played_for_string}: \n Games played: {games_played} \n Points: {points} \n "
+                 f"Assists: {assists} \n Rebounds: {rebounds} \n Steals: {steals} \n Blocks: {blocks} \n "
+                 f"Turnovers: {turnovers} \n Pir: {pir}"
+        )
+
+        # return [SlotSet("player", None)]
+        return [AllSlotsReset()]
     
 class ActionReturnAvailableLeaders(Action):
 
